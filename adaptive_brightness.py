3d6008f28f67e543c8a2de47aa64b249ccabde97
Kyle Corry <kylecorry31@gmail.com>
2018-05-12 12:40:04 -0400

    Add neural network and tensorflow

--- adaptive_brightness.py
+++ adaptive_brightness.py
@@ -4,6 +4,8 @@ from subprocess import call
 import time
 import datetime
 import psutil
+import tensorflow as tf
+tf.enable_eager_execution()
 
 
 def to_range(value, minimum, maximum):
@@ -125,9 +127,48 @@ class SimpleAdaptiveBrightness(AdaptiveBrightness):
             self.set_brightness(light * self.brightness_compensation)
             self.last_change = light
 
+class MLAdaptiveBrightness(AdaptiveBrightness):
+
+    def __init__(self, change_threshold=6, light_sensor=LightSensor(), backlight=Backlight()):
+        AdaptiveBrightness.__init__(self, light_sensor, backlight)
+        self.change_threshold = change_threshold
+        self.last_change = -1
+        self.data = []
+        self.learning_rate = 0.01
+        self.num_steps = 10
+        self.batch_size = 10
+        self.my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=self.learning_rate)
+        self.my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(self.my_optimizer, 5.0)
+        self.model = tf.keras.models.load_model(
+            "model",
+            custom_objects=None,
+            compile=False
+        )
+        self.model.compile(optimizer=self.my_optimizer, loss=tf.keras.losses.mean_squared_error)
+
+
+    def run(self):
+        light = self.get_light()
+        if self.last_change == -1 or abs(light - self.last_change) > self.change_threshold:
+            self.set_brightness(self.model.predict(np.array([light]))[0][0])
+            self.last_change = light
+
+    def learn(self, light, brightness):
+        self.data.append([light, brightness])
+        features = np.array([x[0] for x in self.data])
+        labels = np.array([x[1] for x in self.data])
+        self.model.fit(features, labels, batch_size=self.batch_size, epochs=self.num_steps)
+        tf.keras.models.save_model(
+            self.model,
+            "model",
+            overwrite=True,
+            include_optimizer=False
+        )
+
+
 
 if __name__ == "__main__":
-    adaptive_brightness = SimpleAdaptiveBrightness(0.2)
+    adaptive_brightness = MLAdaptiveBrightness()
     while True:
         adaptive_brightness.run()
         time.sleep(1)
